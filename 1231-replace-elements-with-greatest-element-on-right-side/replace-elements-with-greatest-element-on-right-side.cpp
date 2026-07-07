0792d70ba143a5e0503c83b451cf379110ca3534
Naveen Gupta <naveengupta8010@gmail.com>
2026-07-07 15:42:29 +0530

    Time: 1 ms (52.46%) | Memory: 72.9 MB (21.29%) - LeetSync

--- 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.cpp
+++ 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.cpp
@@ -1,24 +1,16 @@
+// optimal soln in O(N)
 class Solution {
 public:
     vector<int> replaceElements(vector<int>& arr) {
         int n = arr.size();
 
-        if (n == 1) {
-            arr[0] = -1;
-            return arr;
+        int maxi = -1;
+
+        for (int i = n - 1; i >= 0; i--) {
+            int curr = arr[i];
+            arr[i] = maxi;
+            maxi = max(maxi, curr);
         }
-
-        for (int i = 0; i < n - 1; i++) {
-            int leader = arr[i + 1];
-
-            for (int j = i + 1; j < n; j++) {
-                leader = max(leader, arr[j]);
-            }
-
-            arr[i] = leader;
-        }
-
-        arr[n - 1] = -1;
 
         return arr;
     }
