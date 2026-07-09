436faad2b05d0ddc08716fcbaa1dce113ada9c6c
Naveen Gupta <naveengupta8010@gmail.com>
2026-07-09 13:32:48 +0530

    Time: 24 ms (50.96%) | Memory: 228.8 MB (55.07%) - LeetSync

---
+++ 3838-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.cpp
@@ -0,0 +1,63 @@
+class DSU {
+public:
+    vector<int> parent, size;
+
+    DSU(int n) {
+        parent.resize(n);
+        size.resize(n, 1);
+
+        for (int i = 0; i < n; i++)
+            parent[i] = i;
+    }
+
+    int find(int x) {
+        if (parent[x] == x)
+            return x;
+
+        return parent[x] = find(parent[x]);   // Path Compression
+    }
+
+    void unite(int u, int v) {
+        int pu = find(u);
+        int pv = find(v);
+
+        if (pu == pv)
+            return;
+
+        // Union by Size
+        if (size[pu] < size[pv]) {
+            parent[pu] = pv;
+            size[pv] += size[pu];
+        } else {
+            parent[pv] = pu;
+            size[pu] += size[pv];
+        }
+    }
+};
+
+class Solution {
+public:
+    vector<bool> pathExistenceQueries(int n, vector<int>& nums, int maxDiff,
+                                      vector<vector<int>>& queries) {
+
+        DSU dsu(n);
+
+        for (int i = 1; i < n; i++) {
+            if (nums[i] - nums[i - 1] <= maxDiff) {
+                dsu.unite(i, i - 1);
+            }
+        }
+
+        vector<bool> ans;
+
+        // Answer each query
+        for (auto &q : queries) {
+            int u = q[0];
+            int v = q[1];
+
+            ans.push_back(dsu.find(u) == dsu.find(v));
+        }
+
+        return ans;
+    }
+};
