7e888e85297cad95931cce91529ecf46712c7f1f
Naveen Gupta <naveengupta8010@gmail.com>
2026-07-09 15:09:56 +0530

    Time: 18 ms (59.18%) | Memory: 223.9 MB (93.97%) - LeetSync

--- 3838-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.cpp
+++ 3838-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.cpp
@@ -1,64 +1,22 @@
-class DSU {
-public:
-    vector<int> parent, size;
-
-    DSU(int n) {
-        parent.resize(n);
-        size.resize(n, 1);
-
-        for (int i = 0; i < n; i++)
-            parent[i] = i;
-    }
-
-    int find(int x) {
-        if (parent[x] == x)
-            return x;
-
-        return parent[x] = find(parent[x]);   // Path Compression
-    }
-
-    void unite(int u, int v) {
-        int pu = find(u);
-        int pv = find(v);
-
-        if (pu == pv)
-            return;
-
-        // Union by Size
-        if (size[pu] < size[pv]) {
-            parent[pu] = pv;
-            size[pv] += size[pu];
-        } else {
-            parent[pv] = pu;
-            size[pu] += size[pv];
-        }
-    }
-};
-
 class Solution {
 public:
-    vector<bool> pathExistenceQueries(int n, vector<int>& nums, int maxDiff,
-                                      vector<vector<int>>& queries) {
-
-        DSU dsu(n);
-
-        // Connect adjacent indices if their difference <= maxDiff
-        for (int i = 1; i < n; i++) {
-            if (nums[i] - nums[i - 1] <= maxDiff) {
-                dsu.unite(i, i - 1);
+    vector<bool> pathExistenceQueries(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
+        vector<int> compo(n,0);
+        for(int i = 1; i < n; i++){
+            if(nums[i] - nums[i-1] <= maxDiff){
+                // this means this i will belong to same component.
+                compo[i] = compo[i-1];
+            }
+            else{
+                compo[i] = compo[i-1] + 1 ; // reset compo
             }
         }
 
-        vector<bool> ans;
-
-        // Answer each query
-        for (auto &q : queries) {
-            int u = q[0];
-            int v = q[1];
-
-            ans.push_back(dsu.find(u) == dsu.find(v));
+        int q=queries.size(); 
+        vector<bool> ans(q,0); 
+        for (int i = 0; i < q; i++) { 
+            ans[i] = (compo[queries[i][0]] == compo[queries[i][1]]);
         }
-
         return ans;
     }
 };
