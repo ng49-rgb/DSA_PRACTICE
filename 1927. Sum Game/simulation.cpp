56083a5149b1ccb567eb455602b12110493a4f0d
Naveen Gupta <naveengupta8010@gmail.com>
2026-08-23 12:49:51 +0530

    Add files via upload

---
+++ 1927. Sum Game/simulation.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool sumGame(string num) {
+        int n = num.size();
+
+        int sum1 = 0;
+        int sum2 = 0;
+        int q1 = 0;
+        int q2 = 0;
+
+        for(int i = 0; i < n / 2; i++) {
+            if(num[i] == '?')
+                q1++;
+            else
+                sum1 += num[i] - '0';
+        }
+
+        for(int i = n / 2; i < n; i++) {
+            if(num[i] == '?')
+                q2++;
+            else
+                sum2 += num[i] - '0';
+        }
+
+        return ((q1 + q2) % 2 == 1) || (2 * (sum1 - sum2) != 9 * (q2 - q1));
+    }
+};
