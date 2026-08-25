b672750ca8e313475ceea00ad1073cd5cef0abf3
Naveen Gupta <naveengupta8010@gmail.com>
2026-08-25 11:55:35 +0530

    Add files via upload

---
+++ 3718. Smallest Missing Multiple of K/optimal.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int missingMultiple(vector<int>& nums, int k) {
+        unordered_set<int> st(nums.begin(), nums.end());
+
+        for (int x = k; ; x += k) {
+            if (!st.count(x)) {
+                return x;
+            }
+        }
+    }
+};
