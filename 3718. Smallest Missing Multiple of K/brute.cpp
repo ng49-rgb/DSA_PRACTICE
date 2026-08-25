48fac8af7b27ddf9716f05701e6f948179eb582b
Naveen Gupta <naveengupta8010@gmail.com>
2026-08-25 11:55:03 +0530

    Add files via upload

---
+++ 3718. Smallest Missing Multiple of K/brute.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int missingMultiple(vector<int>& nums, int k) {
+        for (int i = k; ; i += k) {
+            if (find(nums.begin(), nums.end(), i) == nums.end()) {
+                return i;
+            }
+        }
+    }
+};
