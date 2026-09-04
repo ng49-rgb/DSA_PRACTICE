43aa9d7e5bf8e7df8f03a4924243454acc51e369
Naveen Gupta <naveengupta8010@gmail.com>
2026-09-04 11:39:05 +0530

    Add files via upload

---
+++ 3903. Smallest Stable Index I/impln.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+        int n = nums.size();
+        for(int i=0; i<n; i++){
+            int maxVal = nums[i], minVal = nums[i];
+
+            for(int j=0; j<i; j++){
+                maxVal = max(maxVal, nums[j]);
+            }
+
+            for(int j=i+1; j<n; j++){
+                minVal = min(minVal, nums[j]);
+            }
+
+            if(maxVal - minVal <= k) return i;
+        }
+        return -1;
+    }
+};
